23d830d9455dfabcbde2c95344e10c5c6e6af0ba
Jeremiah Plummer <5714388+jlplummer@users.noreply.github.com>
2019-12-13 19:54:15 -0500

    Finished refactoring; prototype "complete"

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Generate number pyramid math problems.
 
 ![](example.png)
+
+*Project is an exercise in learning Go*
--- pyramid/pyramid.go
+++ pyramid/pyramid.go
@@ -10,82 +10,35 @@
 package pyramid
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
 
 type Pyramid struct {
 	Pyramid            [][]int
-	Pyramid2           [][]int
 	HiddenRows         [][]int
-	ReversePyramid     [][]int
-	ReversedHiddenRows [][]int
-	PyramidData        map[int]map[string]int
 	ReversePyramidData map[int]map[string]int
 }
 
 func (p *Pyramid) GeneratePyramid(userInt int) *Pyramid {
-	p.populateCells(userInt)
-	p.determineHidden(userInt)
-	p.reversePyramid(userInt)
-	p.reverseHidden(userInt)
-	p.populatePyramidData(userInt)
-	p.reversePyramidData(userInt)
 	p.populateCellsReverse(userInt)
-	fmt.Println("pyramid2", p.Pyramid2)
+	p.determineHidden(userInt)
+	p.reversePyramidData(userInt)
 	return p
 }
 
-func (p *Pyramid) populatePyramidData(userInt int) {
-	pd := make(map[int]map[string]int)
-	var cellId int = 0
-	for j := 0; j < userInt; j++ {
-		for g := 0; g < len(p.Pyramid[j]); g++ {
-			pd[cellId] = map[string]int{"cellId": cellId, "cellValue": p.Pyramid[j][g], "cellHidden": p.HiddenRows[j][g], "cellBreak": 0}
-			cellId++
-		}
-		pd[cellId] = map[string]int{"cellId": cellId, "cellValue": 0, "cellHidden": 0, "cellBreak": 1}
-		cellId++
-	}
-
-	p.PyramidData = pd
-}
-
 func (p *Pyramid) reversePyramidData(userInt int) {
-	rpd := make(map[int]map[string]int)
+	rpd := make(map[int]map[string]int, userInt)
 	cellId := 0
 	for j := 0; j < userInt; j++ {
-		for g := 0; g < len(p.ReversePyramid[j]); g++ {
-			rpd[cellId] = map[string]int{"cellId": cellId, "cellValue": p.ReversePyramid[j][g], "cellHidden": p.ReversedHiddenRows[j][g], "cellBreak": 0}
+		for g := 0; g < len(p.Pyramid[j]); g++ {
+			rpd[cellId] = map[string]int{"cellId": cellId, "cellValue": p.Pyramid[j][g], "cellHidden": p.HiddenRows[j][g], "cellBreak": 0}
 			cellId++
 		}
 		rpd[cellId] = map[string]int{"cellId": cellId, "cellValue": 0, "cellHidden": 0, "cellBreak": 1}
 		cellId++
 	}
 	p.ReversePyramidData = rpd
-}
-
-func (p *Pyramid) reversePyramid(userInt int) {
-	reversedPyramid := make([][]int, userInt)
-	newIndex := 0
-	for j := (userInt - 1); j >= 0; j-- {
-		reversedPyramid[newIndex] = make([]int, len(p.Pyramid[j]))
-		reversedPyramid[newIndex] = p.Pyramid[j]
-		newIndex++
-	}
-	p.ReversePyramid = reversedPyramid
-}
-
-func (p *Pyramid) reverseHidden(userInt int) {
-	rh := make([][]int, userInt)
-	newIndex := 0
-	for j := (userInt - 1); j >= 0; j-- {
-		rh[newIndex] = make([]int, len(p.HiddenRows[j]))
-		rh[newIndex] = p.HiddenRows[j]
-		newIndex++
-	}
-	p.ReversedHiddenRows = rh
 }
 
 func (p *Pyramid) PyramidSize() int {
@@ -100,26 +53,6 @@ func pyramidCell(a, b int) int {
 	return a + b
 }
 
-func (p *Pyramid) populateCells(userInt int) {
-	pyramid := make([][]int, userInt)
-	for x := 0; x < userInt; x++ {
-		innerLen := userInt
-		pyramid[x] = make([]int, (innerLen - x))
-		for j := 0; j < len(pyramid[x]); j++ {
-			if x == 0 {
-				rand.Seed(time.Now().UnixNano())
-				pyramid[x][j] = rand.Intn(100)
-				time.Sleep(100 * time.Millisecond)
-			} else {
-				firstNum := pyramid[x-1][j]
-				secondNum := pyramid[x-1][j+1]
-				pyramid[x][j] = pyramidCell(firstNum, secondNum)
-			}
-		}
-	}
-	p.Pyramid = pyramid
-}
-
 func (p *Pyramid) populateCellsReverse(userInt int) {
 	pyramid := make([][]int, userInt)
 	cntr := 0
@@ -129,7 +62,6 @@ func (p *Pyramid) populateCellsReverse(userInt int) {
 		cntr++
 
 		for j := 0; j < len(pyramid[x]); j++ {
-			fmt.Println("xj", x, j)
 			if x == (userInt - 1) {
 				rand.Seed(time.Now().UnixNano())
 				pyramid[x][j] = rand.Intn(100)
@@ -141,26 +73,15 @@ func (p *Pyramid) populateCellsReverse(userInt int) {
 			}
 		}
 	}
-	p.Pyramid2 = pyramid
+	p.Pyramid = pyramid
 }
 
 func (p *Pyramid) determineHidden(userInt int) {
 	//TODO: this actually isn't determining hidden... it's determining what to show. fix names
-
-	pattern4 := [][]int{{0, 6, 9}, {3, 6, 8}} //TODO: this won't work actually... see notebook
-	pattern5 := [][]int{{0, 1, 7, 9, 11}, {3, 4, 6, 9, 11}}
+	pattern5 := [][]int{{10, 11, 8, 3, 5}, {13, 14, 7, 3, 5}}
 
 	rand.Seed(time.Now().UnixNano())
 	patternToUse := rand.Intn(2)
-
-	var pattern []int
-
-	switch userInt {
-	case 4:
-		pattern = pattern4[patternToUse]
-	case 5:
-		pattern = pattern5[patternToUse]
-	}
 
 	hiddenRows := make([][]int, userInt)
 	cntr := 0
@@ -169,7 +90,7 @@ func (p *Pyramid) determineHidden(userInt int) {
 	for j, g := range p.Pyramid {
 		hiddenRows[j] = make([]int, len(g))
 		for a, _ := range g {
-			for _, l := range pattern {
+			for _, l := range pattern5[patternToUse] {
 				if l == cntr {
 					hiddenRows[j][a] = 1
 				}
